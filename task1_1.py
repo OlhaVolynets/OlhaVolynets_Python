e6ffb09256183ac84c010da97da90f0d3951d206
Olha <olya.volynets.28@gmail.com>
2019-04-30 12:53:24 +0300

    added task1_1

---
+++ task1_1.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+address = input("What is your adress?: ")
+
+print("Hello,", name)
+print("Your age is:", age)
+print("Your address is:", address)
