e6db167f928586d9765d6144c49aa874a6992e21
Olha <olya.volynets.28@gmail.com>
2019-05-01 16:26:36 +0300

    Deleted 3 files(classwork)

--- homework/classwork1.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-
-if a > b:
-    print("biggest number is {}".format(a))
-else:
-    print("biggest number is {}".format(b))
--- homework/classwork2.py
+++
@@ -1,7 +0,0 @@
-num = float(input("Enter number: "))
-
-if (num % 2) == 0:
-    print("Number {} is even number".format(num))
-else:
-    print("Number {} is odd number".format(num))
-
