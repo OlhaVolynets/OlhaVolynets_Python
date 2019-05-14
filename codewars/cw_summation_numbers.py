4e9bce48d2d271641b1d4d54af6bc57af24025b6
Olha <olya.volynets.28@gmail.com>
2019-05-14 18:04:49 +0300

    Added codewars grasshoper summation

---
+++ codewars/cw_summation_numbers.py
@@ -0,0 +1,10 @@
+# Write a program that finds the summation
+# of every number from 1 to num.
+# The number will always be a positive integer greater than 0.
+
+def summation(num):
+    numbers = []
+    for i in range(1, num + 1):
+        # print(i)
+        numbers.append(i)
+    return sum(numbers)
