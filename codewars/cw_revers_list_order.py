534ac82ec09b3afb0e3737318eb2c5a97c77995d
Olha <olya.volynets.28@gmail.com>
2019-05-14 12:29:38 +0300

    Added codewars reverse list order

---
+++ codewars/cw_revers_list_order.py
@@ -0,0 +1,6 @@
+# In this kata you will create a function
+# that takes in a list and returns a list with the reverse order.
+
+def reverse_list(l):
+    reversed_list = l[::-1]
+    return reversed_list
